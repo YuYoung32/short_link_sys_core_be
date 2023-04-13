16ab5aca20534f11014b5af21d7bb444de6cceaa
YuYoung <yuyoung32@qq.com>
2023-04-13 10:57:16 +0800

    feat(/handler/disk): complete disk monitor
    [main-version-0.1.2]
    - Complete disk monitor.

--- handler/monitor/disk.go
+++ handler/monitor/disk.go
@@ -5,10 +5,43 @@
 
 package monitor
 
+import (
+	"github.com/shirou/gopsutil/v3/disk"
+	"short_link_sys_core_be/log"
+)
+
+const devicename = "vda"
+
 var (
-	diskTotal int64
+	diskTotal uint64
 )
 
 func diskStaticInfoSet() {
+	if usageStat, err := disk.Usage(`/`); err == nil {
+		diskTotal = usageStat.Total
+	} else {
+		log.MainLogger.WithField("module", "monitor").Error("diskStaticInfoSet: ", err)
+	}
+}
 
+// diskDynamicInfo 磁盘动态信息 每隔一秒调用
+func diskDynamicInfo() (readBytes, writeBytes, diskUsed, diskFree uint64) {
+	/*
+		dd if=/dev/zero of=tempfile bs=1M count=1024 conv=fdatasync
+		dd if=tempfile of=/dev/null bs=1M count=1024
+	*/
+	ioCounters, err := disk.IOCounters(devicename)
+	if err != nil {
+		log.MainLogger.WithField("module", "monitor").Error("diskDynamicInfo: ", err)
+		return
+	}
+	readBytes = ioCounters[devicename].ReadBytes
+	writeBytes = ioCounters[devicename].WriteBytes
+	if usageStat, err := disk.Usage(`/`); err == nil {
+		diskUsed = usageStat.Used
+		diskFree = usageStat.Free
+	} else {
+		log.MainLogger.WithField("module", "monitor").Error("diskDynamicInfo: ", err)
+	}
+	return
 }
---
+++ handler/monitor/disk_test.go
@@ -0,0 +1,28 @@
+/**
+ * Created by YuYoung on 2023/4/13
+ * Description: 磁盘监控测试
+ */
+
+package monitor
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDiskStaticInfoSet(t *testing.T) {
+	diskStaticInfoSet()
+	t.Log(diskTotal)
+}
+
+func TestDiskDynamicInfo(t *testing.T) {
+	readBytes1, writeBytes1, _, _ := diskDynamicInfo()
+	mbDiv := uint64(1024 * 1024)
+	//gbDiv := mbDiv * 1024
+	for {
+		time.Sleep(time.Second)
+		readBytes2, writeBytes2, used, free := diskDynamicInfo()
+		t.Log((readBytes2-readBytes1)/mbDiv, (writeBytes2-writeBytes1)/mbDiv, used/mbDiv, free/mbDiv)
+		readBytes1, writeBytes1 = readBytes2, writeBytes2
+	}
+}
